802c464bc2815c97e8f8323d0d8d3c0983c53593
yaacov-zerrad <yaazerrad@gmail.com>
2022-11-01 16:41:24 +0200

    success

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import { OnTransactionHandler } from '@metamask/snap-types';
 
 
 export const onTransaction: OnTransactionHandler = async ({ transaction, chainId, }) => {
-  /*  
-  The problem is to call an external API and return the value in the { status } variable
-  To call the API with the address of the smart contract, you can use: url + { tansaction.to }
-  */
 
+  //let address = transaction.to
+  let address = "0x7b69c4f2acf77300025e49dbdbb65b068b2fda7d"
 
-  let status = "To change..."
-
+ return ( async()=> {
+  
+  const response = await fetch('https://coral-app-v2f78.ondigitalocean.app/api/extension?address='+ address);
+  //the response have to be converted to json type file, so it can be used
+  const data = await response.json();
+  const status = data.trustonCertified 
   // Object returner has Metamask and display in TRUSTON tab
   const insights = {
     "Recognize": status,
@@ -19,7 +21,7 @@ export const onTransaction: OnTransactionHandler = async ({ transaction, chainId
     transaction,
     chainId,
   };
-  return { insights };
+  return { insights };})()
 };
   
 
